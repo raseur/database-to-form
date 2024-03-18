8d76e3f1309138dbe8ec5b210ff071e50bdabd23
Antoine J <64862896+raseur@users.noreply.github.com>
2024-03-19 00:01:28 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 <?php
-$host = 'localhost'; // adresse du serveur
+
+$host = 'localhost'; // adresse du serveur sql
 $dbname = 'database'; // nom de la base de données
 $user = 'root'; // utilisateur de la base de données
 $pass = ''; // mot de passe de la base de donnée
